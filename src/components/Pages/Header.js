46dc2fcac6e6b12216c109954bde5c5a74ceb286
iamarbaazkhan <arbaazansarkhan25@gmail.com>
2020-06-20 21:26:04 +0530

    first commit

--- src/components/Pages/Header.js
+++ src/components/Pages/Header.js
@@ -7,9 +7,9 @@ const header=props=>(
                 <div className="masthead-subheading">Hello, I'm Arbaaz</div>
                 <div className="masthead-heading">I am a  Front-End Web developer.</div>
                 <div className="d-flex justify-content-center">
-                <a class="btn btn-primary btn-social mx-3" href="https://www.linkedin.com/in/arbaazansarkhan/" target="_blank"><i class="fab fa-linkedin-in "></i></a>
-                <a class="btn btn-dark btn-social mx-3" href="https://github.com/iamarbaazkhan" target="_blank"><i class="fab fa-github "></i></a>
-                <a class="btn btn-instagram btn-social mx-3" href="https://www.instagram.com/arbaazkhan25/" target="_blank"><i class="fab fa-instagram"></i></a>
+                <a class="btn btn-primary btn-social mx-3" href="https://www.linkedin.com/in/arbaazansarkhan/"><i class="fab fa-linkedin-in "></i></a>
+                <a class="btn btn-dark btn-social mx-3" href="https://github.com/iamarbaazkhan" ><i class="fab fa-github "></i></a>
+                <a class="btn btn-instagram btn-social mx-3" href="https://www.instagram.com/arbaazkhan25/" ><i class="fab fa-instagram"></i></a>
                 </div>
 
             </div>
--- src/components/Pages/Portfolio.js
+++ src/components/Pages/Portfolio.js
@@ -1,6 +1,7 @@
 import React , {Component} from 'react';
 import Portfolioitem from './Portfolioitem.js';
 import './Portfolio.css';
+import {Link} from 'react-scroll';
 //importing image 
 import img1 from '../assets/img/ipfacompany.png';
 import img2 from '../assets/img/androidapp.png';
@@ -12,12 +13,12 @@ import img6 from '../assets/img/androidapp.png';
 // creating a array and map it 
 
 const portfolio =[
-    {title:'Patent  Booking Website',subtitle:'Tech Stack : Wordpress, Html5, Css3',image:img1,link:<a href="https://ipfa.in/"  target="_blank" className="visit"><i className="fas fa-link"></i>  VISIT</a>},
-    {title:'E-Learning Android App',subtitle:'Tech Stack : Core Java, MySql, Php',image:img2, link:<a href="https://play.google.com/store/apps/details?id=com.arbicce.intccemobileapp"  target="_blank" className="visit"><i className="fas fa-link"></i>  VISIT</a>},
-    {title:'CCTV Dealers Website',subtitle:'Tech Stack : Wordpress, Html5, Css3',image:img3,link:<a href="https://trinetratechnologies.co.in/"  target="_blank" className="visit"><i className="fas fa-link"></i>  VISIT</a>},
-    {title:'Event Booking Website',subtitle:'Tech Stack : Wordpress, Html5, Css3',image:img4,link:<a href="https://amanecercreators.com/"  target="_blank" className="visit"><i className="fas fa-link"></i>  VISIT</a>},
-    {title:'E-commerce Website',subtitle:'Made many UI changes,associated with DigitalOn company',image:img5,link:<a href="https://giftingsolutionsindia.com/"  target="_blank" className="visit"><i className="fas fa-link"></i>  VISIT</a>},
-    {title:'PDF Reader Android App',subtitle:'Tech Stack : Core Java, XML',image:img6,link:<a href="https://play.google.com/store/apps/details?id=com.intcce.pocketbook"  target="_blank" className="visit"><i className="fas fa-link"></i>  VISIT</a>}
+    {title:'Patent  Booking Website',subtitle:'Tech Stack : Wordpress, Html5, Css3',image:img1,link:<Link to="https://ipfa.in/"   className="visit"><i className="fas fa-link"></i>  VISIT</Link>},
+    {title:'E-Learning Android App',subtitle:'Tech Stack : Core Java, MySql, Php',image:img2, link:<Link to="https://play.google.com/store/apps/details?id=com.arbicce.intccemobileapp"  className="visit"><i className="fas fa-link"></i>  VISIT</Link>},
+    {title:'CCTV Dealers Website',subtitle:'Tech Stack : Wordpress, Html5, Css3',image:img3,link:<Link to="https://trinetratechnologies.co.in/"   className="visit"><i className="fas fa-link"></i>  VISIT</Link>},
+    {title:'Event Booking Website',subtitle:'Tech Stack : Wordpress, Html5, Css3',image:img4,link:<Link to="https://amanecercreators.com/"   className="visit"><i className="fas fa-link"></i>  VISIT</Link>},
+    {title:'E-commerce Website',subtitle:'Made many UI changes,associated with DigitalOn company',image:img5,link:<Link to="https://giftingsolutionsindia.com/"   className="visit"><i className="fas fa-link"></i>  VISIT</Link>},
+    {title:'PDF Reader Android App',subtitle:'Tech Stack : Core Java, XML',image:img6,link:<Link to="https://play.google.com/store/apps/details?id=com.intcce.pocketbook"  className="visit"><i className="fas fa-link"></i>  VISIT</Link>}
 
     
 ];
