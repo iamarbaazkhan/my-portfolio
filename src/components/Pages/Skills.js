ceb1c3570ead5e0afbcfb8956bb56cf33b679dfb
Arbaaz Ansar Khan <arbaaz.ansar.khan@gmail.com>
2022-11-27 20:17:38 +0530

    remove SQL logo

--- src/components/Pages/Skills.js
+++ src/components/Pages/Skills.js
@@ -45,15 +45,7 @@ const skills = (props) => (
               />
             </a>
           </div>
-          <div className="col-md-3 col-sm-6 my-3">
-            <a href="#!">
-              <img
-                className="img-fluid d-block mx-auto"
-                src="assets/img/logos/sql-img.svg"
-                alt=""
-              />
-            </a>
-          </div>
+          
         </div>
       </div>
     </section>
