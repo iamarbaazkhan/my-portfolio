caf7ec5d1188e8703b2e128774cbb64426dc2d16
Arbaaz Ansar Khan <arbaaz.ansar.khan@gmail.com>
2022-11-27 20:15:28 +0530

    removed instagram logo

--- src/components/Pages/Header.js
+++ src/components/Pages/Header.js
@@ -29,14 +29,7 @@ const header = (props) => (
           >
             <i class="fab fa-github "></i>
           </a>
-          <a
-            class="btn btn-instagram btn-social mx-3"
-            href="https://www.instagram.com/arbaaz.khan98"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i class="fab fa-instagram"></i>
-          </a>
+          
         </div>
       </ScrollAnimation>
     </div>
